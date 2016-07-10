de107ac7bb33ca95ad4ec7e1b9a3f485ed4e2ba0
Jack Mocherman <mochermanjack@gmail.com>
2016-07-10 12:21:58 -0500

    instinct classes appear and disappear on hover event

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,4 +3,11 @@
 var $ = require('jquery');
 const dom = require('./dom');
 
-console.log("test", dom);
+$(document).ready(() => {
+  $('#player1__name').keyup(function() {
+    $('.robot__type').prop('disabled', this.value === "" ? true : false);
+  });
+});
+
+dom.DisableBtns();
+dom.DisplayInstincts();
--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,5 +1,20 @@
 "use strict";
 
-var test = 'test';
+var $ = require('jquery');
+const dom = {};
 
-module.exports = test;
+dom.DisableBtns = () => {
+  $('.robot__type').prop('disabled', true);
+  $('.robot__model').hide();
+};
+
+dom.DisplayInstincts = () => {
+  $('.robot__type__instinct').mouseenter(function() {
+    $('.instinct__model').show();
+  });
+  $('.robot__type__instinct').mouseout(function() {
+    $('.instinct__model').hide();
+  })
+};
+
+module.exports = dom;
